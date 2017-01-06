7b4983deb593531cb6590a6fdaaf7585932cc62a
Altieres de Matos <altitdb@gmail.com>
2017-01-06 17:03:59 -0200

    Update capitulo-02-exercicio-02.sql

--- plsql/respostas/capitulo-02-exercicio-02.sql
+++ plsql/respostas/capitulo-02-exercicio-02.sql
@@ -1,1 +1,9 @@
+ALTER TABLE time MODIFY (ind_ativo NOT NULL);
+
+ALTER TABLE time ADD (CONSTRAINT ck_time_ativo CHECK (UPPER(ind_ativo) = 'S' or UPPER(ind_ativo) = 'N'));
+
+ALTER TABLE time ADD CONSTRAINT uk_nome_time UNIQUE (nome);
+
+CONSTRAINT FOREIGN KEY (id_tecnico) REFERENCES tecnico (id_tecnico),
+CONSTRAINT pk_id_time PRIMARY KEY (id_time);
 
