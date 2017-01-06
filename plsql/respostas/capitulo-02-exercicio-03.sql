a8d1b5f0838e3f3bb339a93338c694c2d41a3596
Altieres de Matos <altitdb@gmail.com>
2017-01-06 17:04:50 -0200

    Update capitulo-02-exercicio-03.sql

--- plsql/respostas/capitulo-02-exercicio-03.sql
+++ plsql/respostas/capitulo-02-exercicio-03.sql
@@ -1,1 +1,4 @@
-
+cOMMENT ON TABLE time IS ‘[CADASTRO] Tabela para armazenamento de times’;
+COMMENT ON COLUMN time.id_time IS ‘Código identificador do time’;
+COMMENT ON COLUMN time.nome IS ‘Nome do time’;
+COMMENT ON COLUMN time.ind_ativo IS ‘Indica se o time está ativo ou  não’;
