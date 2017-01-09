64cc8d958dad4c243d19bd08c12544401e77169d
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-01-08 23:28:14 -0200

    Adicionando Constraints

--- plsql/respostas/capitulo-02-exercicio-02.sql
+++ plsql/respostas/capitulo-02-exercicio-02.sql
@@ -1,9 +1,16 @@
-ALTER TABLE time MODIFY (ind_ativo NOT NULL);
-
-ALTER TABLE time ADD (CONSTRAINT ck_time_ativo CHECK (UPPER(ind_ativo) = 'S' or UPPER(ind_ativo) = 'N'));
-
-ALTER TABLE time ADD CONSTRAINT uk_nome_time UNIQUE (nome);
-
-CONSTRAINT FOREIGN KEY (id_tecnico) REFERENCES tecnico (id_tecnico),
-CONSTRAINT pk_id_time PRIMARY KEY (id_time);
-
+-- TIME
+alter table TIME  add constraint PK_TIME primary key (ID_TIME);
+alter table TIME  add constraint UK_TIME unique (NOME);
+-- JOGADOR
+alter table JOGADOR  add constraint PK_JOGADOR primary key (ID_JOGADOR);
+alter table JOGADOR  add constraint FK_JOGADOR foreign key (ID_TIME)  references time(ID_TIME);
+alter table JOGADOR  add constraint UK_JOGADOR unique (NOME);
+--
+alter table JOGADOR  add constraint CK_JOGADOR  check (SALARIO >= 0);
+--
+-- TECNICO
+alter table TECNICO  add constraint PK_TECNICO primary key (ID_TECNICO);
+alter table TECNICO  add constraint FK_TIME foreign key (ID_TIME)  references time (ID_TIME);
+alter table TECNICO  add constraint UK_TECNICO unique (NOME);
+--
+alter table TECNICO  add constraint CK_TECNICO  check (SALARIO >= 0);
