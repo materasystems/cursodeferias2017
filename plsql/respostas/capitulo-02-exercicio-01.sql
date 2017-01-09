05c5a5c2a4f1763b2f79b5a6ca99f1097d48692d
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-01-08 23:27:59 -0200

    Criando tabelas

--- plsql/respostas/capitulo-02-exercicio-01.sql
+++ plsql/respostas/capitulo-02-exercicio-01.sql
@@ -1,6 +1,22 @@
-CREATE TABLE time
-( id_time NUMBER NOT NULL,
-  nome VARCHAR2(400) NOT NULL,
-  ind_ativo VARCHAR2(1),
-  id_tecnico NUMBER NOT NULL
- ) TABLESPACE treinamento;
+CREATE TABLE JOGADOR 
+(ID_JOGADOR   NUMBER,
+NOME          VARCHAR2(200),
+DT_NASCIMENTO DATE,
+SALARIO       NUMBER(14,2),
+ID_TIME       NUMBER);
+-- 
+alter table JOGADOR modify id_jogador not null;
+alter table JOGADOR modify nome not null;
+alter table JOGADOR modify salario not null;
+alter table JOGADOR modify id_time not null;
+--
+CREATE TABLE TIME 
+(ID_TIME      NUMBER,
+NOME          VARCHAR2(200) NOT NULL);
+--
+CREATE TABLE TECNICO 
+(ID_TECNICO   NUMBER NOT NULL,
+NOME          VARCHAR2(200) NOT NULL,
+DT_NASCIMENTO DATE,
+SALARIO       NUMBER(14,2) NOT NULL,
+ID_TIME       NUMBER NOT NULL);
