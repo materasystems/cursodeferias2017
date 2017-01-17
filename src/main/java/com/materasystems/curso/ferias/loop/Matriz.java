cdd9a992883d798792930ac62028de5da2796f31
Paulo Almeida <palmeida@kenzanmedia.com>
2017-01-17 15:33:39 -0200

    matriz

---
+++ src/main/java/com/materasystems/curso/ferias/loop/Matriz.java
@@ -0,0 +1,55 @@
+/*
+ * MATERA Systems, Curso de Férias 2017 - Maringá
+ */
+package com.materasystems.curso.ferias.loop;
+
+/**
+ * 
+ * @author rochapaulo
+ *
+ */
+public class Matriz {
+
+    private Matriz() {
+        super();
+    }
+    
+    /**
+     * Cria um array multi dimensional (linhas x colunas)
+     * e inicializa todas celulas com -1
+     * 
+     * @param linhas
+     * @param colunas
+     * @return
+     */
+    public static int[][] criaMatriz(int linhas, int colunas) {
+    
+        throw new UnsupportedOperationException("Operação não implementada");
+    }
+    
+    /**
+     * Recuperar o elemento requisitado
+     * 
+     * @param matriz
+     * @param linha
+     * @param coluna
+     * @return
+     */
+    public static int[] getElemento(int[][] matriz, int linha, int coluna) {
+        
+        throw new UnsupportedOperationException("Operação não implementada");
+    }
+
+    /**
+     * Multiplica cada elemento da matriz pelo fator de multiplicação
+     * 
+     * @param matriz
+     * @param fatorMultiplicacao
+     * @return
+     */
+    public static int[][] multiplicaPor(int[][] matriz, int fatorMultiplicacao) {
+
+        throw new UnsupportedOperationException("Operação não implementada");
+    }
+    
+}
---
+++ src/test/java/com/materasystems/curso/ferias/loop/MatrizTest.java
@@ -0,0 +1,76 @@
+/*
+ * MATERA Systems, Curso de Férias 2017 - Maringá
+ */
+package com.materasystems.curso.ferias.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ * @author rochapaulo
+ *
+ */
+public class MatrizTest {
+
+    private static final int[][] MATRIZ_4_X_4 = 
+            new int[][] 
+            {
+                { -1, -1, -1, -1}, 
+                { -1, -1, -1, -1}, 
+                { -1, -1, -1, -1}, 
+                { -1, -1, -1, -1}
+            };
+    
+    private static final int[][] MATRIZ_4_X_4_VARIOS_VALORES = 
+            new int[][] 
+            {
+                { 40,  2, -5, 10}, 
+                { 33, 31, 01, 99}, 
+                { -1, 21, 51, -9}, 
+                { 32, 34, -3, 20}
+            };
+            
+    private static final int[][] MATRIZ_4_X_4_VARIOS_VALORES_X8 = 
+            new int[][] 
+            {
+                { 320,  16, -40, 80}, 
+                { 264, 248, 8, 792 }, 
+                { -8, 168, 408, -72}, 
+                { 256, 272, -24, 60}
+            };
+         
+    /**
+     * Implementar o método criaMatriz da classe #Matriz
+     *         
+     */
+    @Test
+    public void criaMatriz() {
+    
+        int[][] matriz = Matriz.criaMatriz(4, 4);
+        Assert.assertArrayEquals(MATRIZ_4_X_4, matriz);
+    }
+    
+    /**
+     * Implementar o método getElemento da classe #Matriz
+     * 
+     */
+    @Test
+    public void getElemento() {
+    
+        int[] elemento = Matriz.getElemento(MATRIZ_4_X_4_VARIOS_VALORES, 2, 3);
+        Assert.assertEquals(-9, elemento);
+    }
+ 
+    /**
+     * Implementar o método multiplicaPor da classe #Matriz
+     * 
+     */
+    @Test
+    public void multiplicaMatrizPorOito() {
+        
+        int[][] resultado = Matriz.multiplicaPor(MATRIZ_4_X_4_VARIOS_VALORES, 8);
+        Assert.assertArrayEquals(MATRIZ_4_X_4_VARIOS_VALORES_X8, resultado);
+    }
+    
+}
