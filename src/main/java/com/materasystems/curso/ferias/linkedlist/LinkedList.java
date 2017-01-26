818c1ff3eeaf4044dd40006cb622a10d36a152e9
Paulo Almeida <palmeida@kenzanmedia.com>
2017-01-26 16:49:24 -0200

    LinkedList Ref. Impl

---
+++ src/main/java/com/materasystems/curso/ferias/linkedlist/LinkedList.java
@@ -0,0 +1,170 @@
+/*
+ * MATERA Systems, Curso de Férias 2017 - Maringá
+ */
+package com.materasystems.curso.ferias.linkedlist;
+
+import java.io.PrintStream;
+
+/**
+ * LinkedList
+ * 
+ * @author rochapaulo
+ * @since 2017
+ *
+ */
+public class LinkedList<T> {
+
+    /**
+     * LinkedList internal structure, this is used to
+     * store user data and bind each node to the next
+     * making the list.
+     * 
+     * @author paulo
+     *
+     */
+    private class Node {
+        
+        Node(T value) {
+            this.value = value;
+        }
+        
+        final T value;
+        Node next;
+    }
+    
+    private Node head;
+    private Node tail;
+    private int size = 0;
+    
+    /**
+     * Add new value
+     * 
+     * @throws NullPointerException if the argument is null.
+     * @param value
+     */
+    public void add(T value) {
+        
+        if (value == null) {
+            throw new NullPointerException();
+        }
+        
+        if (head == null) {
+            head = new Node(value);
+            head.next = tail;
+            tail = head;
+        } else {
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+        
+        size++;
+    }
+    
+    /**
+     * Removes the element of index
+     * 
+     * @throws IndexOutOfBoundsException if index is higher then size 
+     * or lesser than 0
+     * @param index
+     */
+    public void remove(int index) {
+        
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        Node previous = null;
+        Node current = head;
+        while (index > 0) {
+            previous = current;
+            current = current.next;
+            index--;
+        }
+        previous.next = current.next;
+        size--;
+    }
+
+    /**
+     * Checks if contains
+     * 
+     * @throws NullPointerException if value is null.
+     * @return
+     */
+    public boolean contains(T value) {
+        
+        if (value == null) 
+            throw new NullPointerException();
+        
+        Node current = head;
+        while (current != null) {
+            if (value.equals(current.value)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+    
+    /**
+     * Iterate over the list printing each value
+     * to the provided stream.
+     * 
+     * @throws NullPointerException if stream is null.
+     * @param stream
+     */
+    public void printElements(PrintStream stream) {
+
+        if (stream == null) {
+            throw new NullPointerException();
+        }
+        
+        int i = 0;
+        Node current = head;
+        while (current != null) {
+            stream.println("index: " + i + " - " + current.value);
+            i++;
+        }
+    }
+    
+    /**
+     * get value from index
+     *   
+     * @throws IndexOutOfBoundsException if index is higher then size 
+     * or lesser than 0
+     * @param index
+     * @return
+     */
+    public T get(int index) {
+        
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node aux = head;
+        while (index > 0) {
+            aux = aux.next;
+            index--;
+        }
+        
+        return aux.value;
+    }
+    
+    /**
+     * Size of the list
+     * 
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * Checks if list is empty
+     * 
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+}
---
+++ src/test/java/com/materasystems/curso/ferias/linkedlist/LinkedListTest.java
@@ -0,0 +1,118 @@
+/*
+ * MATERA Systems, Curso de Férias 2017 - Maringá
+ */
+package com.materasystems.curso.ferias.linkedlist;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author rochapaulo
+ * @since 2017
+ *
+ */
+public class LinkedListTest {
+
+    @Test
+    public void empty() {
+     
+        LinkedList<String> list = new LinkedList<>();
+        Assert.assertEquals(0, list.size());
+        Assert.assertTrue(list.isEmpty());
+    }
+    
+    @Test
+    public void adicionar() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Paulo");
+        list.add("Evandro");
+        list.add("MATERA Systems");
+
+        Assert.assertEquals(3, list.size());
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void remover() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Paulo");
+        list.add("Evandro");
+        list.add("MATERA Systems");
+        
+        Assert.assertEquals(3, list.size());
+        
+        list.remove(2);
+        
+        Assert.assertEquals(2, list.size());
+        
+        Assert.assertTrue(list.contains("Evandro"));
+        Assert.assertTrue(list.contains("Paulo"));
+    }
+    
+    @Test
+    public void contains() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Paulo");
+        list.add("Evandro");
+        list.add("MATERA Systems");
+        
+        Assert.assertTrue(list.contains("Evandro"));
+        Assert.assertTrue(list.contains("MATERA Systems"));
+        Assert.assertTrue(list.contains("Paulo"));
+        
+        Assert.assertFalse(list.contains("Existo#SQN"));
+    }
+ 
+    @Test
+    public void getElement() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Paulo");
+        list.add("Evandro");
+        list.add("MATERA Systems");
+        
+        Assert.assertEquals("MATERA Systems", list.get(2));
+        Assert.assertEquals("Evandro", list.get(1));
+        Assert.assertEquals("Paulo", list.get(0));
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void accessNegativeIndex() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.get(-1);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void accessIndexHigherThanSize() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Paulo");
+        list.get(1);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void printElementsNullStream() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.printElements(null);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void containsNullReference() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.contains(null);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void addNullReference() {
+        
+        LinkedList<String> list = new LinkedList<>();
+        list.add(null);
+    }
+    
+}
